c0cc737f335992e008a0fd01f3e1a9781857cea3
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-10-19 12:42:30 +0200

    update build script to support docker and podman

--- sw/build.sh
+++ sw/build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
-/usr/bin/podman run --rm -v `pwd`:/sw -w /sw --name snitch_build ghcr.io/pulp-platform/snitch /bin/bash build_internal.sh $1
+runner=docker
+if ! [ -x "$(command -v docker)" ]; then
+	runner=podman
+fi
+$runner run --rm -v `pwd`:/sw -w /sw --name snitch_build ghcr.io/pulp-platform/snitch /bin/bash build_internal.sh $1
